cc6952f7eebb611f9dac8af241bce2723eec0f85
Marcos Aaron Castro Rivas <maracastror@gmail.com>
2024-07-19 01:08:23 -0600

    fix: Se arregla el bg de section Missin

--- components/Index/Mission.js
+++ components/Index/Mission.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Mission() {
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32 bg-muted">
+    <section className="w-full py-12 md:py-24 lg:py-32">
       <div className="container px-4 md:px-6">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
