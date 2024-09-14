c0954d7aae14ff7920de8662f4d118d72983c45d
Marcos Aaron Castro Rivas <maracastror@gmail.com>
2024-09-13 22:33:25 -0600

    add: Se arregla menu colapsable

--- components/navbar/CollapseMenu.js
+++ components/navbar/CollapseMenu.js
@@ -1,7 +1,10 @@
-'use client'
+"use client";
 
 import { useState } from "react";
-import { ActiveLink } from "../active-link/ActiveLink"
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { TbMenu } from "react-icons/tb";
+import { RxCross1 } from "react-icons/rx";
 
 const navItems = [
   { path: "/quienes-somos", text: "¿Quiénes somos?" },
@@ -12,13 +15,58 @@ const navItems = [
 ];
 
 export default function CollapseMenu() {
+  const pathName = usePathname();
+  const [status, setStatus] = useState(false);
+
+  const handleClick = () => {
+    if (status === false) {
+      setStatus(true);
+    } else {
+      setStatus(false);
+    }
+  }
+
+  const handleNavigation = () => {
+    setStatus(false)
+  }
+
+  console.log(status);
+
   return (
-    <div className={`bg-secondary/90 w-7/12 h-[40vh] text-lg border-primary rounded-md shadow-lg -right-6 top-4 absolute flex flex-col justify-center gap-4 z-50 p-4 visible md:invisible lg:invisible xl:invisible'}`}>
-      {
-        navItems.map((navItem) => (
-          <ActiveLink key={navItem.path} {...navItem} />
-        ))
-      }
-    </div>
-  )
+    <>
+      <div className="relative">
+        <button
+          className={`absolute top-4 ${status === true ? " invisible " : " visible "} md:invisible lg:invisible xl:invisible transition transition-all'`}
+          onClick={handleClick}
+        >
+          <TbMenu />
+        </button>
+
+        <button
+          className={`relative top-4 z-50 ${status === false ? " invisible " : " visible "} md:invisible lg:invisible xl:invisible transition transition-all'`}
+          onClick={handleClick}
+        >
+          <RxCross1 />
+        </button>
+      </div>
+
+      <div
+        className={`bg-secondary/90 w-7/12 h-[40vh] text-lg border-primary rounded-md shadow-lg -right-6 top-4 absolute flex flex-col justify-center gap-4 z-40 p-4 ${
+          status === true ? " visible " : " invisible "
+        } md:invisible lg:invisible xl:invisible transition transition-all'}`}
+      >
+        {navItems.map((navItem) => (
+          <Link
+            href={navItem.path}
+            onClick={handleNavigation}
+            className={`hover:underline  mr-2 transition-all ${
+              pathName == navItem.path && " underline text-orange"
+            }`}
+          >
+            {navItem.text}
+          </Link>
+        ))}
+      </div>
+    </>
+  );
 }
