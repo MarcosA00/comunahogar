96e03b006bf507719cf7b217c52cac02aa536b51
MarcosA00 <85717673+MarcosA00@users.noreply.github.com>
2024-09-22 23:08:11 -0600

    Actualizar Carousel.js

--- components/index/Carousel.js
+++ components/index/Carousel.js
@@ -8,7 +8,7 @@ export default function Carousel() {
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-              Gallería
+              Galería
             </h2>
             <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
               Explora nuestra galería y echa un vistazo a las actividades en las que emos participado.
